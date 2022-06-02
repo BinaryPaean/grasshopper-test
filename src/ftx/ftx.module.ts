65930cc9aad44ed57714cff5e933b240383a0688
Aaron Stewart <aaron@binarypaean.com>
2022-06-01 20:35:52 -0700

    Finally has 'npx typeorm migration:generate' working.

--- src/ftx/ftx.module.ts
+++ src/ftx/ftx.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FtxService } from './ftx.service';
 import { FtxController } from './ftx.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ftx } from '../entities/ftx.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ftx])],
   controllers: [FtxController],
   providers: [FtxService]
 })
--- src/ftx/ftx.service.ts
+++ src/ftx/ftx.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateFtxDto } from './dto/create-ftx.dto';
 import { UpdateFtxDto } from './dto/update-ftx.dto';
+import { Ftx } from '../entities/ftx.entity';
 
 @Injectable()
 export class FtxService {
+  constructor(
+    @InjectRepository(Ftx)
+    private readonly ftxRepository: Repository<Ftx>,) {}
+
   create(createFtxDto: CreateFtxDto) {
     return 'This action adds a new ftx';
   }
